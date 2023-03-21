ed0fc02de23af2cd1835b49ee8cce64330914d8c
Oleksandr-xp28 <128472063+Oleksandr-xp28@users.noreply.github.com>
2023-03-21 12:44:28 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,8 +1,1 @@
-#Домашнє Завдання 1
 
-num1 = int(input("Enter the first digit: "))
-num2 = int(input("Enter the third digit: "))
-num3 = int(input("Enter the first digit: "))
-
-print("Amount of:", num1 + num2 + num3)
-print("product of numbers:", num1 * num2 * num3)
